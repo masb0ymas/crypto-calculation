d334737c95599ed4c0fad51337dcbc8be3794c60
masb0ymas <n.fajri@outlook.com>
2024-07-07 20:35:58 +0700

    fix: update env

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
     APP_NAME: process.env.APP_NAME,
     APP_PREFIX: process.env.APP_PREFIX,
     APP_SITE_URL: process.env.APP_SITE_URL,
+
+    COINGECKO_API_URL: process.env.COINGECKO_API_URL,
+    COINGECKO_API_KEY: process.env.COINGECKO_API_KEY,
   },
   output: "standalone",
 };
--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,16 @@ const APP_NAME = process.env.APP_NAME || "nextjs";
 const APP_PREFIX = process.env.APP_PREFIX || "nextjs";
 const APP_SITE_URL = process.env.APP_SITE_URL || "example.com";
 
+const COINGECKO_API_URL = process.env.COINGECKO_API_URL;
+const COINGECKO_API_KEY = process.env.COINGECKO_API_KEY;
+
 const env = {
   APP_NAME,
   APP_PREFIX,
   APP_SITE_URL,
+
+  COINGECKO_API_URL,
+  COINGECKO_API_KEY,
 };
 
 export default env;

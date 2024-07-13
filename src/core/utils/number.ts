e8b494e03ed73b02f33e8dbec30d0d8f9003a250
masb0ymas <n.fajri@outlook.com>
2024-07-13 19:10:49 +0700

    fix: number format

--- src/core/utils/number.ts
+++ src/core/utils/number.ts
@@ -7,7 +7,7 @@ import _ from "lodash";
  * @param options
  * @returns
  */
-export function formatNumber(
+function _intl_fraction(
   value: number,
   locales?: Intl.LocalesArgument,
   options?: Intl.NumberFormatOptions
@@ -15,11 +15,53 @@ export function formatNumber(
   const localesArgs = locales ? locales : "en-US";
 
   const result = new Intl.NumberFormat(localesArgs, {
-    maximumSignificantDigits: 3,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
     ...options,
   }).format(value);
 
   return result;
+}
+
+/**
+ * 
+ * @param value 
+ * @param locales 
+ * @param options 
+ * @returns 
+ */
+function _intl_number(
+  value: number,
+  locales?: Intl.LocalesArgument,
+  options?: Intl.NumberFormatOptions
+) {
+  const localesArgs = locales ? locales : "en-US";
+
+  const result = new Intl.NumberFormat(localesArgs, {
+    maximumFractionDigits: 4,
+    ...options,
+  }).format(value);
+
+  return result;
+}
+
+/**
+ * 
+ * @param value 
+ * @param locales 
+ * @param options 
+ * @returns 
+ */
+export function intlNumberFormat(
+  value: number,
+  locales?: Intl.LocalesArgument,
+  options?: Intl.NumberFormatOptions
+) {
+  if (value % 1 === 0) {
+    return _intl_fraction(value, locales, options)
+  } else {
+    return _intl_number(value, locales, options)
+  }
 }
 
 /**
@@ -45,9 +87,9 @@ export function validateNumber(value: any): number {
 }
 
 /**
- * 
- * @param value 
- * @returns 
+ *
+ * @param value
+ * @returns
  */
 export function roundDecimal(value: number) {
   const result = Math.round((value + Number.EPSILON) * 100) / 100;
